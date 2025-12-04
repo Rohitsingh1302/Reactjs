83f683412f273af636f43e0e6f144b449091e539
rohit1302 <rohitnew1302@gmail.com>
2025-12-04 23:23:21 +0530

    done

---
+++ lpuclass/src/classcodes/usestate.jsx
@@ -0,0 +1,17 @@
+import React,{useState}  from "react";
+const Child = ({ onClick })=> {console.log("CHILD RENDERED");
+return <button onClick={onClick}>click Child</button>;
+};
+export default function App(){
+    const [count,setCount]=useState(0);
+    const handlechildclick=()=>{
+        console.log("CHILD BNUTTON CLICKED");
+    };
+    return(
+        <>
+        <h1>Count:{count}</h1>
+        <Child onClick={handlechildclick}/>
+        <button onClick={()=>setCount(count+1)}>Increase Parent</button>
+</>
+    )
+}

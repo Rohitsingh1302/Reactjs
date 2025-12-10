81c1711a444f40087d33babf286c964d9daf6088
rohit1302 <rohitnew1302@gmail.com>
2025-12-10 09:43:28 +0530

    done

--- lpuclass/src/services/postapi.jsx
+++ lpuclass/src/services/postapi.jsx
@@ -5,3 +5,21 @@ const api = axios.create({
 export const getPosts = () => {
   return api.get("/posts"); // return promise from axios
 };
+
+//app.jsx ka code:- 
+// import React, { useEffect } from "react";
+// import { getPosts } from "./services/postapi.jsx";
+// export default function App() {
+//   const fetchPosts = async () => {
+//     const res = await getPosts();
+//     console.log(res.data); // <-- API response
+//   };
+//   useEffect(() => {
+//     fetchPosts(); // call API on component mount
+//   }, []);
+//   return (
+//     <div>
+//       <h1>Hellloo</h1>
+//     </div>
+//   );
+// }

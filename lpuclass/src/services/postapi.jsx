3abae81bf758957d2d96b56684224e32f7f73ed9
rohit1302 <rohitnew1302@gmail.com>
2025-12-16 14:46:50 +0530

    done

--- lpuclass/src/services/postapi.jsx
+++ lpuclass/src/services/postapi.jsx
@@ -1,29 +1,107 @@
-import axios from "axios";
-const api = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
+import axios from 'axios';
+
+const api=axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
 });
-export const getPosts = () => {
-  return api.get("/posts"); // return promise from 
-// export const getAllUsers = () => {
-//   return api.get("/users"); // return promise from 
-};
+export const getAllUsers=()=>{
+    return api.get('/users');
+}
+export const getUserById=(id)=>{    
+    return api.get(`/users/${id}`);
+}
+export const createUser=(user)=>{
+    return api.post('/users',user);
+}
+export const deleteUser=(id)=>{ 
+    return api.delete(`/users/${id}`);
+}
+export const editUser=(id,data)=>{
+    return api.put(`/users/${id}`,data);
+}
 
-//app.jsx ka code:- 
+// app.jsx code:-
+// import React, { useEffect, useState } from 'react'
+// import { createUser, getAllUsers } from './services/PostApi'
 
-
-// import React, { useEffect } from "react";
-// import { getPosts } from "./services/postapi.jsx";
 // export default function App() {
-//   const fetchPosts = async () => {
-//     const res = await getPosts();
-//     console.log(res.data); // <-- API response
-//   };
-//   useEffect(() => {
-//     fetchPosts(); // call API on component mount
-//   }, []);
+//   const [users,setUsers]=useState([])
+//   const [isEdit,setIsEdit]=useState(false);
+//   const [user,setUser]=useState({name:"",email:"",phone:""})
+// const handleEdit=(value)=>{
+//   setIsEdit(true)
+//   setUser({name:value.name,email:value.email,phone:value.phone})
+// }
+  
+// const fetchUsers=async()=>{
+//   const res=await getAllUsers();
+//   console.log(res.data)
+//   setUsers(res.data)
+//   setUser({name:"",email:"",phone:""})
+// }
+// const addUser=async()=>{
+//   const res=await createUser(user);
+//   setUsers([...users,res.data])
+// }
+// useEffect(()=>{
+//    fetchUsers()
+// },[])
 //   return (
-//     <div>
-//       <h1>Hellloo</h1>
+//     <><div style={{
+//   display: "flex",
+//   flexDirection: "column",
+//   gap: "20px",
+//   maxWidth: "400px",
+//   margin: "20px auto",
+//   padding: "20px",
+//   border: "1px solid #ccc",
+//   borderRadius: "10px"
+// }}>
+//   <label>Name</label>
+//   <input type='text' value={user.name} onChange={(e)=>setUser({...user,name:e.target.value})}/>
+
+//   <label>Phone</label>
+//   <input type='text' value={user.phone}   onChange={(e)=>setUser({...user,phone:e.target.value})}/>
+
+//   <label>Email</label>
+//   <input type='email' value={user.email}   onChange={(e)=>setUser({...user,email:e.target.value})}/>
+
+// {isEdit ? <button>Edit</button>:<button onClick={addUser}>Add</button>}
+// </div>
+
+
+// {/* CARD CONTAINER ROW WISE */}
+// <div style={{
+//   display: "flex",
+//   flexWrap: "wrap",
+//   gap: "20px",
+//   marginTop: "20px",
+// }}>
+//   {users.map((value) => (
+//     <div key={value.id} style={{
+//       width: "260px",
+//       padding: "15px",
+//       border: "1px solid #ddd",
+//       borderRadius: "12px",
+//       background: "#f9f9f9",
+//       display: "flex",
+//       flexDirection: "column",
+//       gap: "8px",
+//       boxShadow: "0 2px 5px rgba(0,0,0,0.1)"
+//     }}>
+
+//       <h4>UserId: {value.id}</h4>
+//       <p><strong>Name:</strong> {value.name}</p>
+//       <p><strong>Email:</strong> {value.email}</p>
+//       <p><strong>Phone No.:</strong> {value.phone}</p>
+
+//       <div style={{ display: "flex", gap: "10px" }}>
+//         <button onClick={()=>handleEdit(value)}>Edit</button>
+//         <button>Delete</button>
+//       </div>
 //     </div>
-//   );
+//   ))}
+// </div>
+
+//     </>
+//   )
 // }

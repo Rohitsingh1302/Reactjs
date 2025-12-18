3ecc301238bb8d2847ced3449ebd98dee21a78e9
rohit1302 <rohitnew1302@gmail.com>
2025-12-18 23:22:00 +0530

    done

---
+++ lpuclass/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Greet from "./path/to/Greet";
+function App() {
+    return (
+        <div>
+            <Greet />
+        </div>
+    );
+}    
+export default App;
--- lpuclass/src/classcodes/Greet.jsx
+++ lpuclass/src/classcodes/Greet.jsx
@@ -13,14 +13,3 @@ export default Greet;
 //This code defines a React class component named "Greet" that extends the base Component class from the React library. The component's render method returns a JSX fragment containing an <h1> element with the text "GOOD MORNING". When this component is rendered in a React application, it will display the greeting message on the screen. Finally, the component is exported as the default export of the module, allowing it to be imported and used in other parts of the application.
 ///how to use this code
 //To use this Greet component in a React application, you would typically import it into another component or your main application file and include it in the JSX. For example:
-// import React from "react";
-// import Greet from "./path/to/Greet";
-
-// function App() {
-//     return (
-//         <div>
-//             <Greet />
-//         </div>
-//     );
-// }    
-//export default App;

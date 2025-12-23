74c18b22af82127c0045b06a2c138c44f7d11b41
rohit1302 <rohitnew1302@gmail.com>
2025-12-23 22:57:56 +0530

    changes into the main.jsx file

--- lpuclass/src/main.jsx
+++ lpuclass/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//     <StrictMode>
+//     <App />
+//     </StrictMode>
+// )
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore } from 'redux' 
+import { todoReducer } from './redux/todoReducer' 
+
+const store = createStore(todoReducer)
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-    <App />
-    </StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
 )

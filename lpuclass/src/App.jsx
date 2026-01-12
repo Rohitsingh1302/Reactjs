90d95c49001de3f6909d13659fc43a0e0acf2520
rohit1302 <rohitnew1302@gmail.com>
2026-01-12 09:51:21 +0530

    done with etp of react

--- lpuclass/src/App.jsx
+++ lpuclass/src/App.jsx
@@ -1,37 +1,9 @@
-import { useReducer, useState } from 'react'
-import todoReducer from './redux/ToDoReducer'
-function App() {
-  const [state, dispatch] = useReducer(todoReducer, { todos: [] })
-  const [task, setTask] = useState("")
-  
-  const addTask = () => {
-    dispatch({
-      type: 'ADD_TASK',
-      payload: task
-    })
-    setTask("")
-  }
-
-  const deleteTask = (id) => {
-    dispatch({
-      type: 'DELETE_TASK',
-      payload: id
-    })
-  }
-  
-  return(
-    <div>
-      <h2>Task Manager</h2>
-      <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
-      <button onClick={addTask}>Add Task</button>
-      <h3>All Tasks: </h3>
-      <ul>{state.todos.map((item, idx) => 
-        <li key={idx}> 
-        {idx}{item} <button onClick={() => deleteTask(idx)}>Delete Task</button>
-        </li>)}
-      </ul>
-    </div>
+import ShoppingCart from "./classcodes/etp";; 
+function App(){
+  return (   
+      <>
+      <ShoppingCart/>
+      </>  
   )
 }
-
 export default App;
---
+++ lpuclass/src/classcodes/helloworld.jsx
@@ -0,0 +1,9 @@
+function Hello(){
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen bg-blue-50">
+            <h1 className="text-4xl font-bold text-blue-600 mb-4">Hello World</h1>
+            <p className="text-lg text-gray-700">My name is Rohit</p>
+        </div>
+    )
+};
+export default Hello;
